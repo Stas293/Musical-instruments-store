28c9afc0f0f79bcd11ad0fc6597fbb08e22be1d6
Stas293 <iskor29@gmail.com>
2022-04-29 15:16:46 +0300

    Updated SQL script

--- Untitled.sql
+++ Untitled.sql
@@ -4,10 +4,10 @@ USE Musical_instruments_store;
 DELIMITER $$
 
 DROP PROCEDURE IF EXISTS INSERT_ORDER_LIST$$
-CREATE DEFINER=root@localhost PROCEDURE INSERT_ORDER_LIST (IN title VARCHAR(50) CHARSET utf8, IN user_id BIGINT(20), IN status_id BIGINT(20), IN closed BOOLEAN, OUT nid BIGINT(20) UNSIGNED)  BEGIN
+CREATE DEFINER=root@localhost PROCEDURE INSERT_ORDER_LIST (IN user_id BIGINT(20), IN title VARCHAR(50) CHARSET utf8, IN status_id BIGINT(20), IN closed BOOLEAN, OUT nid BIGINT(20) UNSIGNED)  BEGIN
 
-INSERT INTO order_list(title, user_id, status_id, closed) 
-VALUES (title, user_id, status_id, closed);
+INSERT INTO order_list(user_id, title, status_id, closed) 
+VALUES (user_id, title, status_id, closed);
 
 SELECT LAST_INSERT_ID() into nid;
 
@@ -23,18 +23,9 @@ SELECT LAST_INSERT_ID() into nid;
 END$$
 
 DROP PROCEDURE IF EXISTS INSERT_INSTRUMENT_LIST$$
-CREATE DEFINER=root@localhost PROCEDURE INSERT_INSTRUMENT_LIST (IN description VARCHAR(255) CHARSET utf8, IN title VARCHAR(50) CHARSET utf8, IN status_id BIGINT(20), IN closed BOOLEAN, OUT nid BIGINT(20) UNSIGNED)  BEGIN
-INSERT INTO instrument_list (description, title, status_id, closed) 
-VALUES (description, title, status_id, closed);
-
-SELECT LAST_INSERT_ID() into nid;
-
-END$$
-
-DROP PROCEDURE IF EXISTS INSERT_PRICE_LIST$$
-CREATE DEFINER=root@localhost PROCEDURE INSERT_PRICE_LIST (IN instrument_id BIGINT(20), IN price DECIMAL(17, 0), OUT nid BIGINT(20) UNSIGNED)  BEGIN
-INSERT INTO price_list (instrument_id, price) 
-VALUES (instrument_id, price);
+CREATE DEFINER=root@localhost PROCEDURE INSERT_INSTRUMENT_LIST (IN description VARCHAR(255) CHARSET utf8, IN title VARCHAR(50) CHARSET utf8, IN status_id BIGINT(20), IN price DECIMAL(17, 2), IN closed BOOLEAN, OUT nid BIGINT(20) UNSIGNED)  BEGIN
+INSERT INTO instrument_list (description, title, status_id, price, closed) 
+VALUES (description, title, status_id, price, closed);
 
 SELECT LAST_INSERT_ID() into nid;
 
@@ -49,33 +40,17 @@ UPDATE user_list
 END$$
 
 DROP PROCEDURE IF EXISTS UPDATE_INSTRUMENT_LIST$$
-CREATE DEFINER=root@localhost PROCEDURE UPDATE_INSTRUMENT_LIST (IN instrument_id bigint(20), IN description VARCHAR(255) CHARSET utf8, IN title VARCHAR(50) CHARSET utf8, IN status_id BIGINT(20), IN closed BOOLEAN)  BEGIN
+CREATE DEFINER=root@localhost PROCEDURE UPDATE_INSTRUMENT_LIST (IN instrument_id bigint(20), IN description VARCHAR(255) CHARSET utf8, IN title VARCHAR(50) CHARSET utf8, IN status_id BIGINT(20), IN price DECIMAL(17, 2), IN closed BOOLEAN)  BEGIN
 UPDATE instrument_list 
-	SET description = description, title = title, status_id = status_id, closed = closed
-	WHERE instrument_id = instrument_id;
-
-END$$
-
-DROP PROCEDURE IF EXISTS UPDATE_ORDER_LIST$$
-CREATE DEFINER=root@localhost PROCEDURE UPDATE_ORDER_LIST (IN order_id bigint(20), IN title VARCHAR(50) CHARSET utf8, IN status_id BIGINT(20), IN user_id BIGINT(20), IN total_sum DECIMAL(17, 0), IN closed BOOLEAN)  BEGIN
-UPDATE order_list 
-	SET title = title, user_id = user_id, status_id = status_id, total_sum = total_sum, closed = closed
-	WHERE order_id = order_id;
-
-END$$
-
-DROP PROCEDURE IF EXISTS UPDATE_PRICE_LIST$$
-CREATE DEFINER=root@localhost PROCEDURE UPDATE_PRICE_LIST (IN instrument_id BIGINT(20), IN price DECIMAL(17, 0))  BEGIN
-UPDATE price_list
-	SET price = price
+	SET description = description, title = title, status_id = status_id, price = price, closed = closed
 	WHERE instrument_id = instrument_id;
 
 END$$
 
 DROP PROCEDURE IF EXISTS DELETE_ORDER_LIST$$
-CREATE DEFINER=root@localhost PROCEDURE DELETE_ORDER_LIST (IN title VARCHAR(50) CHARSET utf8)  BEGIN
+CREATE DEFINER=root@localhost PROCEDURE DELETE_ORDER_LIST (IN order_id bigint(20))  BEGIN
 
-DELETE FROM order_list WHERE title = title;
+DELETE FROM order_list WHERE order_id = order_id;
 
 END$$
 
@@ -93,11 +68,17 @@ DELETE FROM instrument_list WHERE title = title;
 
 END$$
 
-DROP FUNCTION IF EXISTS FUNC_MAX_ORDER_LIST$$
-CREATE FUNCTION FUNC_MAX_ORDER_LIST(total_sum DECIMAL(17, 0))
-RETURNS decimal(17, 0) deterministic
+DROP FUNCTION IF EXISTS FUNC_COUNT_RATING$$
+CREATE FUNCTION FUNC_COUNT_RATING(rating bigint)
+RETURNS BIGINT UNSIGNED DETERMINISTIC
 BEGIN
-	RETURN MAX(total_sum);
+
+	DECLARE rat BIGINT UNSIGNED default 0;
+    SELECT count(*) into rat
+		from order_history
+        where rating = rat;
+	RETURN rat;
+    
 END$$
 
 DELIMITER ;
@@ -168,14 +149,11 @@ DROP TABLE IF EXISTS order_history;
 CREATE TABLE IF NOT EXISTS order_history (
 	history_id 		bigint(20) 		unsigned NOT NULL AUTO_INCREMENT,
 	date_created 	TIMESTAMP 		DEFAULT 	CURRENT_TIMESTAMP,
-    date_modified 	TIMESTAMP 		DEFAULT 	CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-	description 	varchar(255) 	COLLATE utf8_unicode_ci NOT NULL,
-	title 			varchar(255) 	COLLATE utf8_unicode_ci NOT NULL,
 	user_id  		bigint(20) 		unsigned 	NOT NULL,
-	total_sum 		DECIMAL(17, 0) 	NOT NULL DEFAULT '0',
+	total_sum 		DECIMAL(17, 2) 	NOT NULL DEFAULT '0',
+	title 			varchar(50) 	COLLATE utf8_unicode_ci NOT NULL,
 	rating			bigint(20) 		unsigned 	NOT NULL,
 	status_id 		bigint(20) 		unsigned 	NOT NULL,
-    UNIQUE KEY 		order_history_idx1 	(title),
 	CONSTRAINT 		fk_order_history_status FOREIGN KEY (status_id) references status(status_id),
     CONSTRAINT 		fk_order_history_user FOREIGN KEY (user_id) references user_list(user_id),
     CONSTRAINT		pk_order_history 	PRIMARY KEY (history_id)
@@ -190,6 +168,7 @@ CREATE TABLE IF NOT EXISTS instrument_list (
 	description 	varchar(255) 	COLLATE utf8_unicode_ci NOT NULL,
 	title 			varchar(50) 	COLLATE utf8_unicode_ci NOT NULL,
 	status_id 		bigint(20) 		unsigned NOT NULL,
+    price 			DECIMAL(17, 2) 	NOT NULL DEFAULT '0',
 	closed 			tinyint(1) 		NOT NULL 	DEFAULT '1',
     UNIQUE KEY 		instrument_list_idx1 	(title),
     UNIQUE KEY 		instrument_list_idx2 	(description),
@@ -198,56 +177,25 @@ CREATE TABLE IF NOT EXISTS instrument_list (
 )ENGINE=InnoDB AUTO_INCREMENT=179 DEFAULT CHARSET=utf8
 COLLATE=utf8_unicode_ci;
 
-DROP TABLE IF EXISTS price_list;
-CREATE TABLE IF NOT EXISTS price_list (
-	instrument_id 	bigint(20) 		unsigned NOT NULL,
-    price_id 		bigint(20) 		unsigned NOT NULL AUTO_INCREMENT,
-	price 			DECIMAL(17, 0) 	NOT NULL DEFAULT '0',
-	date 			TIMESTAMP 		DEFAULT 	CURRENT_TIMESTAMP,
-    CONSTRAINT		pk_price_list 	PRIMARY KEY (price_id),
-    CONSTRAINT 		fk_price_list_instrument_list FOREIGN KEY (instrument_id) references instrument_list(instrument_id)
-)ENGINE=InnoDB AUTO_INCREMENT=179 DEFAULT CHARSET=utf8
-COLLATE=utf8_unicode_ci;
-
 DROP TABLE IF EXISTS order_list;
 CREATE TABLE IF NOT EXISTS order_list (
 	order_id 		bigint(20) 		unsigned NOT NULL AUTO_INCREMENT,
 	date_created 	TIMESTAMP 		DEFAULT 	CURRENT_TIMESTAMP,
-	date_updated 	TIMESTAMP 		DEFAULT 	CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-	title 			varchar(50) 	COLLATE utf8_unicode_ci NOT NULL,
 	user_id 		bigint(20) 		unsigned NOT NULL ,
+	title 			varchar(50) 	COLLATE utf8_unicode_ci NOT NULL,
 	status_id 		bigint(20) 		unsigned NOT NULL ,
-	total_sum 		DECIMAL(17, 0) 	NOT NULL DEFAULT '0',
 	closed 			tinyint(1) 		NOT NULL 	DEFAULT '1',
-	UNIQUE KEY 		order_list_idx1 	(title),
     CONSTRAINT 		fk_order_list_user_list FOREIGN KEY (user_id) references user_list(user_id),
     CONSTRAINT 		fk_order_list_status FOREIGN KEY (status_id) references status(status_id),
     CONSTRAINT		pk_order_list 	PRIMARY KEY (order_id)
 )ENGINE=InnoDB AUTO_INCREMENT=179 DEFAULT CHARSET=utf8
 COLLATE=utf8_unicode_ci;
 
-DROP TRIGGER IF EXISTS ORDER_LIST_AFTER_UPDATE;
-DELIMITER $$
-CREATE TRIGGER ORDER_LIST_AFTER_UPDATE
-	AFTER UPDATE 
-    ON order_list 
-    FOR EACH ROW 
-    BEGIN
-		DECLARE n_id bigint(20);
-
-		INSERT INTO order_history(description, title, user_id, total_sum, status_id)
-		VALUES(instrument_list.description, NEW.title, NEW.user_id, NEW.total_sum, NEW.status_id);
-
-	END
-$$
-
-DELIMITER ;
-
 DROP TABLE IF EXISTS instrument_order;
 CREATE TABLE IF NOT EXISTS instrument_order (
 	instrument_id 	bigint(20) 		unsigned NOT NULL ,
 	order_id 		bigint(20) 		unsigned NOT NULL ,
-	price 			DECIMAL(17, 0) 	NOT NULL DEFAULT '0',
+	price 			DECIMAL(17, 2) 	NOT NULL DEFAULT '0',
 	quantity 		DECIMAL(17, 0) 	NOT NULL DEFAULT '0',
 	CONSTRAINT 		fk_instrument_order_instrument_list FOREIGN KEY (instrument_id) references instrument_list(instrument_id),
 	CONSTRAINT 		fk_instrument_order_order_list FOREIGN KEY (order_id) references order_list(order_id),
